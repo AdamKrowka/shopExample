ffc8b55850518e5ce2e6b55095a4cabfa49a9cc4
Adam96 <adamkrowka108@gmail.com>
2020-07-09 23:31:55 +0200

    Updates

---
+++ precache-manifest.f5fbccc59030dbfe342de4d40307ebfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3aa1f6d2fcee3e285f00dd737adfbd",
+    "url": "/shopExample/index.html"
+  },
+  {
+    "revision": "348f6467cf8e145eb5d2",
+    "url": "/shopExample/static/css/main.b3f5f183.chunk.css"
+  },
+  {
+    "revision": "af5494c9d9e90fb876c7",
+    "url": "/shopExample/static/js/2.8e631e2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopExample/static/js/2.8e631e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348f6467cf8e145eb5d2",
+    "url": "/shopExample/static/js/main.cf22f1a3.chunk.js"
+  },
+  {
+    "revision": "eab9be28ca41a5855b62",
+    "url": "/shopExample/static/js/runtime-main.3303066d.js"
+  },
+  {
+    "revision": "17bb76867b6db97d1847a7dc13b6d4c7",
+    "url": "/shopExample/static/media/banner.17bb7686.jpg"
+  }
+]);
